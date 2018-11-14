eaa3a20cbc16d7980193f67bd71cdfd5e5874d0d
ldp1114 <851298348@qq.com>
2018-11-14 13:57:12 +0800

    vghjj

---
+++ src/com/wfd360/TestDemo.java
@@ -0,0 +1,10 @@
+package com.wfd360;
+
+/**
+ * Created by Administrator on 2018/11/14.
+ */
+public class TestDemo {
+      public void method(){
+          System.out.println("----------------");
+        }
+}
