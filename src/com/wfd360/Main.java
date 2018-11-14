c41c70b0bb3350bd8e71cd80daf33a98e8516780
ldp1114 <851298348@qq.com>
2018-11-14 14:00:17 +0800

    vghjj

--- src/com/wfd360/Main.java
+++ src/com/wfd360/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("--------dthdfghdfghfh-------");
+        System.out.println("--------dthdfghdfghfh--222-----");
     }
 }
