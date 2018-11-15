bd31d45af31a9e35f00733c88eb83127a88ce909
ldp1114 <851298348@qq.com>
2018-11-15 11:16:32 +0800

    分支2提交

---
+++ src/com/wfd360/Branch2.java
@@ -0,0 +1,10 @@
+package com.wfd360;
+
+/**
+ * Created by Administrator on 2018/11/15.
+ */
+public class Branch2 {
+      public void method(){
+          System.out.println("--------b2----");
+        }
+}
