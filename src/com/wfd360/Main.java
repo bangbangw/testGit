7ca396b26dcef5e0227d5c95f943f1979d8283a6
wfd360 <389170268@qq.com>
2019-05-10 17:18:55 +0800

    不需要提交的文件以过滤

--- src/com/wfd360/Main.java
+++ src/com/wfd360/Main.java
@@ -3,7 +3,8 @@ package com.wfd360;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("E:\\gitWork\\gitTest01");
+        System.out.println("为了测试：IDEA GIT 忽略文件最佳方式");
     }
 }
