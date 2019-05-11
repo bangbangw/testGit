2d091a7179fa478547ee94870dc463dc3f640f10
0511 <851298348@qq.com>
2019-05-11 10:30:14 +0800

    开发中提交---

--- src/com/wfd360/Main.java
+++ src/com/wfd360/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("-----项目上传测试------------");
+        System.out.println("-----开发中------------");
     }
 }
