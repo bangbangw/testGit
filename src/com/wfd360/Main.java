343059775a88f2e64a3de5965b372c60bd5068d8
0511 <851298348@qq.com>
2019-05-11 10:58:46 +0800

    下插曲后，提交

--- src/com/wfd360/Main.java
+++ src/com/wfd360/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("-----项目上传测试------------");
         System.out.println("-----开发中------------");
         System.out.println("-----开发中2------------");
+        System.out.println("-----中间来了和小插曲--现在接着开发------------");
     }
 
 }
